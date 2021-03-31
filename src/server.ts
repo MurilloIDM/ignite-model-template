d3c16d45234ad6dcc6b1c237a6c747256ef9ee6a
MurilloIDM <murilloidm@gmail.com>
2021-03-31 07:44:55 -0300

    Update: Adiciona lib uuid

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,4 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
-
 app.listen(3000, () => console.log("Server is running!"));
